b455f3406ffbf1ea1e80f240329fe9a217838278
Paul Vinell <paul.vinell@hemnet.se>
2025-08-25 09:42:18 +0200

    Dummy GUI

--- src/emotional_n_back/main.py
+++ src/emotional_n_back/main.py
@@ -1,5 +1,6 @@
 import typer
 
+from emotional_n_back.game import DualNBackPygame
 from emotional_n_back.nback import DualNBackTerminal
 
 app = typer.Typer()
@@ -20,6 +21,21 @@ def cli():
         seed=42,
         show_truth=True,
     ).play()
+
+
+@app.command()
+def gui():
+    DualNBackPygame(
+        length=20,
+        n=1,
+        visual_items=9,
+        auditory_items=8,
+        repeat_probability=0.3,
+        seed=42,
+        stim_ms=1000,
+        isi_ms=500,
+        show_feedback_ms=300,
+    ).run()
 
 
 if __name__ == "__main__":
