3d378563a904a77bf60aa35213fbd66ae6f4a3a3
Paul Vinell <paul.vinell@hemnet.se>
2025-09-05 11:16:08 +0200

    Remove black images

--- src/emotional_n_back/main.py
+++ src/emotional_n_back/main.py
@@ -38,7 +38,7 @@ def dual(
     game = EmotionalDualNBack(
         length=length,
         n=n,
-        repeat_probability=0.25,
+        repeat_probability=0.35,
         seed=seed,
         stim_ms=stim_ms,
         isi_ms=500,
@@ -118,6 +118,17 @@ def kdef():
     for item in kdef_src.iterdir():
         shutil.move(str(item), kdef_dst / item.name)
 
+    # Remove black images
+    black_images = {
+        "neutral": ["48_13", "42_30"],
+        "disgust": ["63_33", "116_32"],
+    }
+    for emotion, img_basenames in black_images.items():
+        for img_basename in img_basenames:
+            img_path = kdef_dst / emotion / f"{img_basename}.jpg"
+            if img_path.exists():
+                print(f"Removing {img_path}...")
+                img_path.unlink()
     # Clean up Kaggle datasets directory
     if kaggle_datasets_dir.exists():
         shutil.rmtree(kaggle_datasets_dir)
